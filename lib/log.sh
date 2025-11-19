2a2dd852ca29f728819aa47faa76ae7ef97b7b93
Vasista <v.adupa@rug.nl>
2025-11-19 10:19:38 +0100

    [Mac] logging test

--- lib/log.sh
+++ lib/log.sh
@@ -1,33 +1,10 @@
 #!/usr/bin/env bash
 # shellcheck shell=bash
 
-: "${SSC_LOG_LEVEL:=info}"
+: "${VERBOSE:=false}"
 
-_log_level_to_num() {
-  case "$1" in
-    error) echo 0 ;;
-    warn)  echo 1 ;;
-    info)  echo 2 ;;
-    debug) echo 3 ;;
-    *)     echo 2 ;;
-  esac
+log_debug() {
+  $VERBOSE || return 0
+  # exactly one string argument is expected
+  printf 'DEBUG: %s\n' "$1" >&2
 }
-
-_log_enabled() {
-  local current wanted
-  current="$(_log_level_to_num "${SSC_LOG_LEVEL:-info}")"
-  wanted="$(_log_level_to_num "$1")"
-  (( current >= wanted ))
-}
-
-_log_print() {
-  # $1 = level label (ERROR/WARN/INFO/DEBUG), rest = message parts
-  local level="$1"; shift
-  local IFS=' '          # force space-separated join regardless of global IFS
-  printf '%s: %s\n' "$level" "$*" >&2
-}
-
-log_error() { _log_enabled error || return 0; _log_print "ERROR" "$@"; }
-log_warn()  { _log_enabled warn  || return 0; _log_print "WARN"  "$@"; }
-log_info()  { _log_enabled info  || return 0; _log_print "INFO"  "$@"; }
-log_debug() { _log_enabled debug || return 0; _log_print "DEBUG" "$@"; }
