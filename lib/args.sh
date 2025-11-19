d2069ba3f4f44c7c77d490ce1055b669c0ddf1f2
Vasista <v.adupa@rug.nl>
2025-11-19 14:21:17 +0100

    [Mac] parse_state function added which will be used to do the state filtering

--- lib/args.sh
+++ lib/args.sh
@@ -1,3 +1,21 @@
 #!/usr/bin/env bash
+# shellcheck disable=SC2034
+args::parse_state() {
+    local var
+    val="$(tr '[:upper:]' '[:lower:]' <<<"$1")"
 
-# args.sh - Args library module
+    case "$val" in
+        dependency|dep|deps)
+            STATE_FILTER="PENDING"
+            REASON_FILTER="Dependency"
+            ;;
+
+        pending|running|suspended|completed|cancelled|failed|timeout|node_fail|preempted|boot_fail|deadline|out_of_memory|completing|configuring|resizing|resv_del_hold|requeued|requeue_fed|requeue_hold|revoked|signaling|special_exit|stage_out|stopped)
+            STATE_FILTER="$(tr '[:lower:]' '[:upper:]' <<<"$val")"
+            ;;
+
+        *)
+            die 2 "Unknown state: $val"
+            ;;
+    esac
+}
